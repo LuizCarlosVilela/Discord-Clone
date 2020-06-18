b6ee5aa6d7ed84073b9c8e91da6546caad4a15fc
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-06-18 18:34:46 -0300

    Criando menu lateral do discord

---
+++ src/components/ServerButton/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+import { Props } from '.';
+
+export const Button = styled.button<Props>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+
+    width: 48px;
+    height: 48px;
+
+    border-radius: 50%;
+    
+    margin-bottom: 8px;
+
+    background-color: ${(props) => 
+        props.isHome ? 'var(--rocketseat)' : 'var(--primary)'};
+
+    position: relative;
+    cursor: pointer;
+
+`;
