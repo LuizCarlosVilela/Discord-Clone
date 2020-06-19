3ed7690e6c2681ca5cc25671fb3f9ff21a989c55
Luiz Carlos <luizcarlosvilela@outlook.com.br>
2020-06-19 12:30:43 -0300

    Terminando parte superior do site, o header

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container} from './styles';
+
+const ChannelList: React.FC = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+};
+
+export default ChannelList;
